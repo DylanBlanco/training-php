32684b2ca0c6d11469ff25515f7efb4285c9f87d
DylanBlanco <dylanblanco1198@gmail.com>
2025-03-19 02:48:40 +0100

    Ciclo for

--- about.php
+++ about.php
@@ -9,7 +9,7 @@
     <title>About</title>
 
     <!-- CSS -->
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./css/style.css">
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -34,8 +34,11 @@
                 </div>
             </div>
         </div>
-        <div class="container mb-3 bg-success bg-gradient">
-            <div class="row">
+        <div class="container mb-3">
+            <div class="row gap-3">
+                <?php
+                    for ($i = 0; $i < $columnsNumber; $i++) {
+                ?>
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
                         <p class="mb-3">
@@ -58,11 +61,14 @@
                         </p>
                     </div>
                 </div>
+                <?php
+                    }
+                ?>
             </div>
         </div>
     </main>
 
-    <div class="container bg-success bg-gradient">
+    <!-- <div class="container bg-success bg-gradient">
         <div class="row">
             <?php
                 for ($i = 0; $i < $columnsNumber; $i++) {
@@ -74,6 +80,6 @@
                 }
             ?>
         </div>
-    </div>
+    </div> -->
 </body>
 </html>
